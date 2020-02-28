432c5afc48545364e11d5a624d563562c480bb35
CharlotteFallices <zhuyuhang_school@163.com>
2020-02-28 12:50:41 +0800

    Update Song.swift

--- Music/Music/Song.swift
+++ Music/Music/Song.swift
@@ -7,18 +7,17 @@
 //
 
 import Foundation
-import SwiftUI
 
 public class Song {
     public var id:Int
     public var title:String
     public var time:Time
-    public var image:Image?
+    public var image:SwiftUI.Image?
     public var singer:[String]
     public var album:String
     public var like:Bool
     public var str:[Str]
-    public init(id:Int,title:String,time:Time,image:Image?,singer:[String],album:String,like:Bool,str:[Str]){
+    public init(id:Int,title:String,time:Time,image:SwiftUI.Image?,singer:[String],album:String,like:Bool,str:[Str]){
         self.id=id
         self.title=title
         self.time=time
